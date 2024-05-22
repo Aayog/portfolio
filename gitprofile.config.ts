f66655bbd7c9e868302ca8c948d7cdaafc3f4570
Aayog Koirala <1aayogkoirala@gmail.com>
2024-05-22 01:34:07 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -9,12 +9,12 @@ const CONFIG = {
     github: {
       display: true,
       header: 'Github Projects',
-      mode: 'automatic',
+      mode: 'manual',
       automatic: {
         sortBy: 'pinned', // or 'updated' to sort by most recent
-        limit: 8,
+        limit: 12,
         exclude: {
-          forks: false,
+          forks: true,
           projects: [],
         },
       },
@@ -172,21 +172,69 @@ const CONFIG = {
     id: '',
     snippetVersion: 6,
   },
+  
   themeConfig: {
-    defaultTheme: 'dark',
+    defaultTheme: 'lofi',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
     disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
     respectPrefersColorScheme: false,
+
+    // Display the ring in Profile picture
     displayAvatarRing: true,
+
+    // Available themes. To remove any theme, exclude from here.
     themes: [
       'light',
       'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+      'procyon',
     ],
+
+    // Custom theme, applied to `procyon` theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
   },
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
   enablePWA: true,
 };
 
